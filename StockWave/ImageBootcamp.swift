60043b6e22e59c0cbf637d6176a27336205b3f32
LightningNemesis <ak.kumar220919@gmail.com>
2024-04-14 00:49:48 -0700

    Added Images & Frames tutorial

---
+++ StockWave/ImageBootcamp.swift
@@ -0,0 +1,18 @@
+//
+//  ImageBootcamp.swift
+//  StockWave
+//
+//  Created by Abhishek Kumar on 4/14/24.
+//
+
+import SwiftUI
+
+struct ImageBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ImageBootcamp()
+}
