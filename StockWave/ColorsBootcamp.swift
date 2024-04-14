44655f2cd6af800dc11247ae657e1191d1f6e528
LightningNemesis <ak.kumar220919@gmail.com>
2024-04-14 00:00:49 -0700

    Added Colors, Shapes, Text

---
+++ StockWave/ColorsBootcamp.swift
@@ -0,0 +1,18 @@
+//
+//  ColorsBootcamp.swift
+//  StockWave
+//
+//  Created by Abhishek Kumar on 4/13/24.
+//
+
+import SwiftUI
+
+struct ColorsBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ColorsBootcamp()
+}
---
+++ StockWave/ShapesBootcamp.swift
@@ -0,0 +1,18 @@
+//
+//  ShapesBootcamp.swift
+//  StockWave
+//
+//  Created by Abhishek Kumar on 4/13/24.
+//
+
+import SwiftUI
+
+struct ShapesBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ShapesBootcamp()
+}
