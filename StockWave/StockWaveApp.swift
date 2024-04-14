8e8ac5772e82dc51b996788629dafd57de76cdae
LightningNemesis <ak.kumar220919@gmail.com>
2024-04-13 22:01:01 -0700

    Initial Commit

---
+++ StockWave/StockWaveApp.swift
@@ -0,0 +1,17 @@
+//
+//  StockWaveApp.swift
+//  StockWave
+//
+//  Created by Abhishek Kumar on 4/13/24.
+//
+
+import SwiftUI
+
+@main
+struct StockWaveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
