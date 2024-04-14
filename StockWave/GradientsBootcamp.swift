bd80f201c29c74ee777021326c5e5fb7bd50d3f2
LightningNemesis <ak.kumar220919@gmail.com>
2024-04-14 00:30:30 -0700

    Added Gradients, Icons tutorial (SF Symbols)

---
+++ StockWave/GradientsBootcamp.swift
@@ -0,0 +1,18 @@
+//
+//  GradientsBootcamp.swift
+//  StockWave
+//
+//  Created by Abhishek Kumar on 4/14/24.
+//
+
+import SwiftUI
+
+struct GradientsBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GradientsBootcamp()
+}
---
+++ StockWave/IconsBootcamp.swift
@@ -0,0 +1,18 @@
+//
+//  IconsBootcamp.swift
+//  StockWave
+//
+//  Created by Abhishek Kumar on 4/14/24.
+//
+
+import SwiftUI
+
+struct IconsBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    IconsBootcamp()
+}
