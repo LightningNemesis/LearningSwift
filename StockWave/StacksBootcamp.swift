7480a25b74dfc1c3b13f51b00970bd283d47d8de
LightningNemesis <ak.kumar220919@gmail.com>
2024-04-14 01:53:59 -0700

    Added Background and Stacks tutorial

---
+++ StockWave/StacksBootcamp.swift
@@ -0,0 +1,18 @@
+//
+//  StacksBootcamp.swift
+//  StockWave
+//
+//  Created by Abhishek Kumar on 4/14/24.
+//
+
+import SwiftUI
+
+struct StacksBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StacksBootcamp()
+}
