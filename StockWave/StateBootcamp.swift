c8fe1f7dbece7c9376b9a95567414ee7225b3283
LightningNemesis <ak.kumar220919@gmail.com>
2024-04-14 18:50:41 -0700

    Added State updates via Button actions and updating Views by observing state via "@State" tag

---
+++ StockWave/StateBootcamp.swift
@@ -0,0 +1,18 @@
+//
+//  StateBootcamp.swift
+//  StockWave
+//
+//  Created by Abhishek Kumar on 4/14/24.
+//
+
+import SwiftUI
+
+struct StateBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StateBootcamp()
+}
