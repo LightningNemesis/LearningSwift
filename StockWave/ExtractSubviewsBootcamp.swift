e0e9869981b35f63e1a9ca7b73dde210e6758b45
LightningNemesis <ak.kumar220919@gmail.com>
2024-04-14 20:34:49 -0700

    Learnt how to seperate Views, extract Subviews and when to do what

---
+++ StockWave/ExtractSubviewsBootcamp.swift
@@ -0,0 +1,18 @@
+//
+//  ExtractSubviewsBootcamp.swift
+//  StockWave
+//
+//  Created by Abhishek Kumar on 4/14/24.
+//
+
+import SwiftUI
+
+struct ExtractSubviewsBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ExtractSubviewsBootcamp()
+}
