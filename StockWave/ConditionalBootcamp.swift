ae27d71c1b701184b46af92b902036398edf8910
LightningNemesis <ak.kumar220919@gmail.com>
2024-04-14 21:11:44 -0700

    Added Conditional Statements & ternary Operators tutorial

---
+++ StockWave/ConditionalBootcamp.swift
@@ -0,0 +1,18 @@
+//
+//  ConditionalBootcamp.swift
+//  StockWave
+//
+//  Created by Abhishek Kumar on 4/14/24.
+//
+
+import SwiftUI
+
+struct ConditionalBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ConditionalBootcamp()
+}
