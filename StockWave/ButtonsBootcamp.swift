13a4d834e857d11602aec63bf70f5ec924ae791e
LightningNemesis <ak.kumar220919@gmail.com>
2024-04-14 18:42:12 -0700

    Added Different types of buttons tutorial

---
+++ StockWave/ButtonsBootcamp.swift
@@ -0,0 +1,18 @@
+//
+//  ButtonsBootcamp.swift
+//  StockWave
+//
+//  Created by Abhishek Kumar on 4/14/24.
+//
+
+import SwiftUI
+
+struct ButtonsBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ButtonsBootcamp()
+}
