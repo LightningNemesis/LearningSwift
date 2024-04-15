f8c375ef09145a8cc5cf32107b8aaf4955403c2b
LightningNemesis <ak.kumar220919@gmail.com>
2024-04-14 18:59:12 -0700

    Learnt how to extract functions & subviews

---
+++ StockWave/ExtractedFunctionsBootcamp.swift
@@ -0,0 +1,18 @@
+//
+//  ExtractedFunctionsBootcamp.swift
+//  StockWave
+//
+//  Created by Abhishek Kumar on 4/14/24.
+//
+
+import SwiftUI
+
+struct ExtractedFunctionsBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ExtractedFunctionsBootcamp()
+}
