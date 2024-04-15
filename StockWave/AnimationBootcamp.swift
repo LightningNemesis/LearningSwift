513374ea5b3c119854c8ab7cba0b8a08f45febc2
LightningNemesis <ak.kumar220919@gmail.com>
2024-04-14 22:39:16 -0700

    Added Animations & Animation timings tutorial

---
+++ StockWave/AnimationBootcamp.swift
@@ -0,0 +1,18 @@
+//
+//  AnimationBootcamp.swift
+//  StockWave
+//
+//  Created by Abhishek Kumar on 4/14/24.
+//
+
+import SwiftUI
+
+struct AnimationBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AnimationBootcamp()
+}
