ed04f20179295dab26a99fc82325faeb62c3c066
LightningNemesis <ak.kumar220919@gmail.com>
2024-04-26 13:09:29 -0700

    Learnt how to use Alerts and Lists (with delete, reorder functionalities inside a Navigation View)

---
+++ StockWave/AlertsBootcamp.swift
@@ -0,0 +1,18 @@
+//
+//  AlertsBootcamp.swift
+//  StockWave
+//
+//  Created by Abhishek Kumar on 4/26/24.
+//
+
+import SwiftUI
+
+struct AlertsBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AlertsBootcamp()
+}
---
+++ StockWave/ListBootcamp.swift
@@ -0,0 +1,18 @@
+//
+//  ListBootcamp.swift
+//  StockWave
+//
+//  Created by Abhishek Kumar on 4/26/24.
+//
+
+import SwiftUI
+
+struct ListBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ListBootcamp()
+}
