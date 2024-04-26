099fb58ba8083647452ed325ebed7b1408fbe9a9
LightningNemesis <ak.kumar220919@gmail.com>
2024-04-25 22:43:02 -0700

    Added navigation view & navigation links

---
+++ StockWave/NavigationViewBootcamp.swift
@@ -0,0 +1,18 @@
+//
+//  NavigationViewBootcamp.swift
+//  StockWave
+//
+//  Created by Abhishek Kumar on 4/25/24.
+//
+
+import SwiftUI
+
+struct NavigationViewBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NavigationViewBootcamp()
+}
