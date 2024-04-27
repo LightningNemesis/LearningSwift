779844af24dd5692e5a601dbddcf98ffb6859cab
LightningNemesis <ak.kumar220919@gmail.com>
2024-04-26 18:07:51 -0700

    Learnt how to use App Storage to persist data using SwiftUI's key value store DB & onAppear on various components

---
+++ StockWave/AppStorageBootcamp.swift
@@ -0,0 +1,18 @@
+//
+//  AppStorageBootcamp.swift
+//  StockWave
+//
+//  Created by Abhishek Kumar on 4/26/24.
+//
+
+import SwiftUI
+
+struct AppStorageBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AppStorageBootcamp()
+}
